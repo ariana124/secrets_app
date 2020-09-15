e251894e7fdd8b22083aa4ee8208f76df90162ed
ariana124 <ariana.bibiano@gmail.com>
2020-09-14 21:23:21 -0700

    required mongoose and created new user schema

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
