af6786545d8c5278ad2e73ee67517d4afa006059
ariana124 <ariana.bibiano@gmail.com>
2020-10-12 13:03:47 -0700

    added hash encryption

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -45,7 +46,7 @@ app.post("/register", function(req, res) {
 
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
